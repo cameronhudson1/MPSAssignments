cf8aac21c545106446b6e81c3b5c61754072bdf7
cmrnhdudson@gmail.com <cmrnhudson@gmail.com>
2021-03-28 14:30:12 -0400

    Stubbed MPI file headers

--- Assignment1/project/src/mpi_hh.c
+++ Assignment1/project/src/mpi_hh.c
@@ -1,1 +1,34 @@
-// Some basic info to edit this file!
+/*
+  Multiple Processor Systems. Spring 2009 (again, Spring 2010)
+  Professor Muhammad Shaaban
+  Author: Dmitri Yudanov (update: Dan Brandt)
+  
+  This is a Hodgkin Huxley (HH) simplified compartamental neuron model 
+*/
+
+#include "plot.h"
+#include "lib_hh.h"
+#include "cmd_args.h"
+#include "constants.h"
+
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+
+// Define macros based on compilation options. This is a best practice that
+// ensures that all code is seen by the compiler so there will be no surprises
+// when a flag is/isn't defined. Any modern compiler will compile out any
+// unreachable code.
+#ifdef PLOT_SCREEN
+  #define ISDEF_PLOT_SCREEN 1
+#else
+  #define ISDEF_PLOT_SCREEN 0
+#endif
+
+#ifdef PLOT_PNG
+  #define ISDEF_PLOT_PNG 1
+#else
+  #define ISDEF_PLOT_PNG 0
+#endif
