d74bc61c1cd8098bb2dab7f9611b19cb68129a24
cmrnhdudson@gmail.com <cmrnhudson@gmail.com>
2021-04-08 18:15:09 -0400

    Added changes to new project

--- Assignment2/project/src/master.cpp
+++ Assignment2/project/src/master.cpp
@@ -36,6 +36,12 @@ void masterMain(ConfigData* data)
             masterSequential(data, pixels);
             stopTime = MPI_Wtime();
             break;
+        case PART_MODE_STATIC_STRIPS_HORIZONTAL:
+            //Call the function that will handle this.
+            startTime = MPI_Wtime();
+            masterStaticStripsHorizontal(data, pixels);
+            stopTime = MPI_Wtime();
+            break;
         default:
             std::cout << "This mode (" << data->partitioningMode;
             std::cout << ") is not currently implemented." << std::endl;
@@ -91,4 +97,43 @@ void masterSequential(ConfigData* data, float* pixels)
     std::cout << "Total Communication Time: " << communicationTime << " seconds" << std::endl;
     double c2cRatio = communicationTime / computationTime;
     std::cout << "C-to-C Ratio: " << c2cRatio << std::endl;
+}
+
+void masterStaticStripsHorizontal(ConfigData* data, float* pixels)
+{
+    int height = data.height;
+    int rank = data.mpi_rank;
+    int procs = data.mpi_procs;
+
+    float *mypixels = NULL;
+    if((mypixels = malloc((height/procs) * sizeof(float))) == NULL)
+    {
+        // Malloc Error
+        exit();
+    }
+
+    /* Render the scene. */
+    // Iterate over rows for this partition
+    for( int row = ( (height/procs) * rank ); row < ( (height/procs) * (rank + 1) ); ++row )
+    {
+        // Iterate over all cols (strips span width)
+        for( int col = 0; col < data.width; ++col )
+        {
+            //Calculate the index into the array.
+            int baseIndex = 3 * ( row * data.width + col );
+
+            //Call the function to shade the pixel.
+            shadePixel(&(mypixels[baseIndex]), row, col, &data);
+        }
+    }
+
+    //Stop the timing.
+    clock_t stop = clock();
+
+    //Figure out how much time was taken.
+    float time = (float)(stop - start) / (float)CLOCKS_PER_SEC;
+    std::cout << "Execution Time: " << time << " seconds" << std::endl << std::endl;
+
+    /* Recieve slave process computations */
+
 }
