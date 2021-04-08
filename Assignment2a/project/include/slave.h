32938e61e9c13f2a1eff5ecd7c4251fb06f1394b
cmrnhdudson@gmail.com <cmrnhudson@gmail.com>
2021-04-08 17:53:33 -0400

    Changed to 2a

---
+++ Assignment2a/project/include/slave.h
@@ -0,0 +1,9 @@
+#ifndef __SLAVE_PROCESS_H__
+#define __SLAVE_PROCESS_H__
+
+#include "RayTrace.h"
+
+void slaveMain( ConfigData *data );
+slaveStaticStripsHorizontal(ConfigData* data, float* pixels);
+
+#endif
