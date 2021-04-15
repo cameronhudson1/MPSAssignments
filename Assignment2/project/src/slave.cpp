e9399d4fb0a7ec36bb345a1b2ff08dddcfbacb50
cmrnhdudson@gmail.com <cmrnhudson@gmail.com>
2021-04-15 19:26:03 -0400

    fixed slave logic

--- Assignment2/project/src/slave.cpp
+++ Assignment2/project/src/slave.cpp
@@ -13,7 +13,8 @@ void slaveMain(ConfigData* data)
     //Depending on the partitioning scheme, different things will happen.
     //You should have a different function for each of the required 
     //schemes that returns some values that you need to handle.
-    float *pixels;
+    float* pixels = new float[3 * ((data->width * data->height)/data->procs)];
+
     switch (data->partitioningMode)
     {
         case PART_MODE_NONE:
@@ -34,23 +35,30 @@ void slaveMain(ConfigData* data)
             break;
         }
     }
+
+    delete[] pixels; 
 }
 
 
 void slaveStaticStripsHorizontal(ConfigData* data, float* pixels)
 {
-    for( int i = 0; i < data->height; ++i )
-    {
-        for( int j = 0; j < data->width; ++j )
-        {
-            int row = i;
-            int column = j;
+    int height = data->height;
+    int width = data->width;
+    int rank = data->mpi_rank;
+    int procs = data->mpi_procs;
+    float strip_width = width/procs;
 
+    // Iterate over rows for this partition
+    for( int col = ( (width/procs) * rank ); col < ( (width/procs) * (rank + 1) ); ++col )
+    {
+        // Iterate over all cols (strips span width)
+        for( int row = 0; row < data->height; ++row )
+        {
             //Calculate the index into the array.
-            int baseIndex = 3 * ( row * data->width + column );
+            int baseIndex = 3 * ( row * width + col );
 
             //Call the function to shade the pixel.
-            shadePixel(&(pixels[baseIndex]),row,j,data);
+            shadePixel(&(pixels[baseIndex]), row, col, data);
         }
     }
 }
