de68ecfe874fbf106b2c593cb6f1dd6ff97fa80c
Anthony Peter Bacchetta <axb6771@mpssubmit.rc.rit.edu>
2021-04-15 12:05:48 -0400

    added some code to slave

--- Assignment2/project/src/slave.cpp
+++ Assignment2/project/src/slave.cpp
@@ -20,24 +20,37 @@ void slaveMain(ConfigData* data)
             //The slave will do nothing since this means sequential operation.
             break;
         case PART_MODE_STATIC_STRIPS_HORIZONTAL:
-            {
+        {
 	    //Call the function that will handle this.
             double startTime = MPI_Wtime();
             slaveStaticStripsHorizontal(data, pixels);
             double stopTime = MPI_Wtime();
             break;
-	    }
+	      }
         default:
-	{
+        {
             std::cout << "This mode (" << data->partitioningMode;
             std::cout << ") is not currently implemented." << std::endl;
             break;
-	}
+	      }
     }
 }
 
 
 void slaveStaticStripsHorizontal(ConfigData* data, float* pixels)
 {
-    
+    for( int i = 0; i < data->height; ++i )
+    {
+        for( int j = 0; j < data->width; ++j )
+        {
+            int row = i;
+            int column = j;
+
+            //Calculate the index into the array.
+            int baseIndex = 3 * ( row * data->width + column );
+
+            //Call the function to shade the pixel.
+            shadePixel(&(pixels[baseIndex]),row,j,data);
+        }
+    }
 }
