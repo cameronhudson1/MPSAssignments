b8ca9dde5faf056928d3cfec4c41a0826e0f099c
cmrnhdudson@gmail.com <cmrnhudson@gmail.com>
2021-04-15 18:59:34 -0400

    Removed redundant directories

--- Assignment2a/project/include/slave.h
+++
@@ -1,9 +0,0 @@
-#ifndef __SLAVE_PROCESS_H__
-#define __SLAVE_PROCESS_H__
-
-#include "RayTrace.h"
-
-void slaveMain( ConfigData *data );
-slaveStaticStripsHorizontal(ConfigData* data, float* pixels);
-
-#endif
