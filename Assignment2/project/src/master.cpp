c3ed65eb33be1c948c4bab45f8ac193964ee3807
Anthony Peter Bacchetta <axb6771@mpssubmit.rc.rit.edu>
2021-04-22 12:22:58 -0400

    added to master

--- Assignment2/project/src/master.cpp
+++ Assignment2/project/src/master.cpp
@@ -188,8 +188,9 @@ void masterStaticBlock(ConfigData* data, float* pixels){
     int width = data->width;
     int rank = data->mpi_rank;
     int procs = data->mpi_procs;
-    float block_width = width/procs;
-    float block_height = height/procs;
+    float factor = sqrt(procs)
+    float block_width = width/procs * factor;
+    float block_height = height/procs * factor;
     
      if(block_width != (int)block_width || block_height != (int)block_height)
     {
@@ -201,10 +202,10 @@ void masterStaticBlock(ConfigData* data, float* pixels){
     
     /* Render the scene. */
     // Iterate over rows for this partition
-    for( int col = ( (block_width) * rank ); col < ( (block_width) * (rank + 1) ); ++col )
+    for( int col = ( (block_width) * rank ); col < ( (block_width) * (rank % factor) ); ++col )
     {
         // Iterate over all cols (strips span width)
-        for( int row = ( (block_height) * rank ); row < ( (block_height) * (rank + 1) ); ++row )
+        for( int row = ( (block_height) * rank ); row < ( (block_height) * (rank % factor) ); ++row )
         {
             //Calculate the index into the array.
             int baseIndex = 3 * ( row * width + col );
